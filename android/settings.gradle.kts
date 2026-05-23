e0ed78444bbd79f5b90f8242bc7e6c4a834180e3
lirsw77-wq <lirsw77@gmail.com>
2026-05-23 13:42:46 +0800

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,11 +1,29 @@
+pluginManagement {
+    val flutterSdkPath = System.getenv("FLUTTER_SDK_PATH") ?: run {
+        val properties = java.util.Properties()
+        val localPropertiesFile = file("local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.inputStream().use { properties.load(it) }
+        }
+        properties.getProperty("flutter.sdk")
+            ?: throw GradleException("flutter.sdk not found in environment or local.properties")
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    // 强制锁定版本，并添加对 AGP 9 的兼容处理
     id("com.android.application") version "8.1.1" apply false
     id("com.android.library") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
 
-// 恢复你原有的加载逻辑
-includeBuild("${settingsDir.parent}/packages/flutter_tools/gradle")
+rootProject.name = "question_app"
 include(":app")
