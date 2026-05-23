59ef4e620a55f6f76cab5f4af6a6b7105134e794
lirsw77-wq <lirsw77@gmail.com>
2026-05-23 11:05:42 +0800

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,16 +1,11 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    // 强制锁定版本，并添加对 AGP 9 的兼容处理
     id("com.android.application") version "8.1.1" apply false
     id("com.android.library") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
 
+// 恢复你原有的加载逻辑
+includeBuild("${settingsDir.parent}/packages/flutter_tools/gradle")
 include(":app")
