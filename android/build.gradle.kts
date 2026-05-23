5e11667b361422a71d96f41abb16d3a1003c3e15
lirsw77-wq <lirsw77@gmail.com>
2026-05-23 13:43:33 +0800

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,38 +1,5 @@
 plugins {
-    id("com.android.application")
-    // ⚠️ 这里已经移除了会导致报错的 kotlin-android 插件
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    namespace = "com.example.question_app" // ⚠️ 请务必把双引号里的内容改成你的实际包名！
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    defaultConfig {
-        applicationId = "com.example.question_app" // ⚠️ 请务必把双引号里的内容改成你的实际包名！
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-}
-
-flutter {
-    source = "../.."
-}
-
-dependencies {
-    // 基础依赖，交给 Flutter 自动管理
+    id("com.android.application") version "8.1.1" apply false
+    id("com.android.library") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
